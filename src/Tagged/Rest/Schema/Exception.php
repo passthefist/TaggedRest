0a0a873bb6a66a9be52dfd3f13fc1f1d0d8af226
Rob Goetz <rgoetz@tagged.com>
2014-02-12 13:06:17 -0800

    rm for readd alter

--- src/Tagged/Rest/Schema/Exception.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace \Tagged\Rest\Schema;
-
-class InvalidParamsException extends \Klein\ValidationException {
-    public function __construct($errorMap) {
-        $this->message = $errorMap['message'];
-    }
-
-}
--- src/Tagged/Rest/Schema/InvalidParamsException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Tagged\Rest\Schema;
-
-class InvalidParamsException extends \Exception {
-    public function __construct($errorMap) {
-        $this->message = $errorMap[0]->message;
-    }
-
-}
